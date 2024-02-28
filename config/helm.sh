de91e46a85cd13cb7e254dd7bc8145384fedb203
maxenceng <maxence.grosjean@cpe.fr>
2024-02-28 01:01:07 +0100

    Added ArgoCD apps config

--- config/helm.sh
+++ config/helm.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 helm repo add stable https://charts.helm.sh/stable
-helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
 helm repo add tailscale https://pkgs.tailscale.com/helmcharts
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
